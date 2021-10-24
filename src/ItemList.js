550a8b24dd23509c505a63c47260849e4d477879
GoodOleJonyCage <adil_ahsan_khan@yahoo.com>
2021-10-23 22:54:51 -0400

    youtube api

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,10 +1,15 @@
+import {useState} from 'react'
+import {GetSearchResults} from './Youtubeservice'
 import './ItemList.css'
+
 import { NavLink } from 'react-router-dom';
 
 export const ItemList = () => {
 
+    //const[lst,setlist]= useState([]);
+    GetSearchResults().then( x => console.log(x));
     var lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21,22,23,24,25,26,27,28,29,30,31,32,33];
-    return lst.map(i => {
+    return  lst.map(i => {
         return <div className="content-item">
             <NavLink to="/detail"  >
                 <img src={"https://picsum.photos/200/300?random=" + i}></img>
